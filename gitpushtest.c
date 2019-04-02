280e7b2e12210949201aae7c6c659c2203966537
golla-lokesh <lokesh.gollauk@gmail.com>
2019-04-02 19:42:39 +0100

    second change to c file

--- gitpushtest.c
+++ gitpushtest.c
@@ -3,9 +3,9 @@
 
 void main(){
 /*git clone and push test*/
-//char txt[]="Ola git hub";
-char txt[9];
-gets(&txt[0]);
+char txt[]="Ola git hub";
+//char txt[9];
+//gets(&txt[0]);
 printf("\t%c%c%c\n\t\t",txt[0],txt[1]-32,txt[2]-32);
 puts(&txt[4]);
 }
