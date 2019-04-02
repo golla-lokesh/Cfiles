06157aaf5dcd3391a0aa579803a3035c1bbdc69a
golla-lokesh <lokesh.gollauk@gmail.com>
2019-04-02 19:38:25 +0100

    first change to c file

--- gitpushtest.c
+++ gitpushtest.c
@@ -1,8 +1,11 @@
+
 #include<stdio.h>
 
 void main(){
 /*git clone and push test*/
-
-printf("HELLO GIT HUB\n");
-puts("hola");
+//char txt[]="Ola git hub";
+char txt[9];
+gets(&txt[0]);
+printf("\t%c%c%c\n\t\t",txt[0],txt[1]-32,txt[2]-32);
+puts(&txt[4]);
 }
